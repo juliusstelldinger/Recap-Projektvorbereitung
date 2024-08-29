da9c9e1f688a55716e62dbb2219a62bb3b853ab4
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2024-08-29 11:45:40 +0200

    feature: Add ec2 deployment

---
+++ 04-iac/terraform/main.tf
@@ -0,0 +1,58 @@
+
+
+# Importiere die VPC-ID und die öffentlichen Subnet-IDs aus dem VPC-Deployment
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "techstarter-tom-iac"
+    key    = "ec2-example/vpc.tfstate"
+    region = var.aws_region
+  }
+}
+
+# Erstelle eine Security Group, die HTTP-Zugriff zulässt
+resource "aws_security_group" "http" {
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-http"
+  }
+}
+
+
+
+# Erstelle eine EC2-Instance
+resource "aws_instance" "web" {
+  ami                    = var.instance_ami # Ubuntu Server 20.04 LTS für eu-central-1
+  instance_type          = var.aws_instance_type
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet_id_1a # Nutzt ein öffentliches Subnetz
+  vpc_security_group_ids = [aws_security_group.http.id]                                # Nutzt VPC-Security-Gruppen
+  key_name               = var.key_pair_id
+
+  tags = {
+    Name = "web-server"
+  }
+}
+

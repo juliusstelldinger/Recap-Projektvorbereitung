8dcd75c006fdb63d954d0a4f4aed4214ac2bc6db
SirKaktor <juliuststelldinger@web.de>
2024-09-19 08:18:27 +0200

    update

--- 02-backend-mini/index.js
+++ 02-backend-mini/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const { PORT } = process.env;
+const { PORT } = 5050;
 
 app.get("/", function (req, res) {
   res.send("Hello World!!?");
